8606bdc41dfa971ff697ac2e9f9c79d3c2f3c8e4
ffmammadov <farid.mammadov@pashabank.az>
2019-07-10 12:54:24 +0400

    Implementing Player interface

---
+++ src/main/java/com/github/ffmammadov/behavioral/strategy/interfaces/player/Mp3Player.java
@@ -0,0 +1,18 @@
+package com.github.ffmammadov.behavioral.strategy.interfaces.player;
+
+import java.nio.charset.StandardCharsets;
+
+public class Mp3Player implements Player {
+
+    private byte[] music;
+
+    public Mp3Player(byte[] music) {
+        this.music = music;
+    }
+
+    @Override
+    public void play() {
+        //Assume We have music decoding & playing algorithm here
+        System.out.println("Playing music: " + new String(music, StandardCharsets.UTF_8));
+    }
+}
