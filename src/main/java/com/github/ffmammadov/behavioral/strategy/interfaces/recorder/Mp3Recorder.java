8910ee3d1b943154be7c2d4d53a9f0ad98af9279
ffmammadov <farid.mammadov@pashabank.az>
2019-07-10 12:54:54 +0400

    Implementing Recorder interface

---
+++ src/main/java/com/github/ffmammadov/behavioral/strategy/interfaces/recorder/Mp3Recorder.java
@@ -0,0 +1,10 @@
+package com.github.ffmammadov.behavioral.strategy.interfaces.recorder;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class Mp3Recorder implements Recorder {
+    @Override
+    public byte[] record(String name) {
+        return String.join(".", name, "mp3").getBytes(UTF_8);
+    }
+}
