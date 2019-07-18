322152335498f4dd4e41d2450ce92725889ec4c6
ffmammadov <farid.mammadov@pashabank.az>
2019-07-18 20:28:10 +0400

    Decorator pattern in action!

--- behavioral/decorator/src/main/java/com/github/ffmammadov/decorator/BarService.java
+++ behavioral/decorator/src/main/java/com/github/ffmammadov/decorator/BarService.java
@@ -2,6 +2,7 @@ package com.github.ffmammadov.decorator;
 
 public class BarService {
     public static void main(String[] args) {
-
+        Cocktail boom = new Vodka(new Tequila(new Scotch(new Cola())));
+        System.out.println(boom.order());
     }
 }
